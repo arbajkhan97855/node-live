813eac68e3a457b974490ab212931aa48c75138c
arbajkhan97855 <pathanarbaj03328@gmail.com>
2025-08-01 23:16:17 +0530

    first commit

--- index.js
+++ index.js
@@ -160,9 +160,9 @@ app.post("/booking", async (req, res) => {
         const bkdata = new Booking(req.body);
         const datavalue = await bkdata.save()
         if (datavalue) {
-            res.status(200).json({ message: "User created successfully" });
+            res.status(200).json({ message: "Your booking successfully" });
         } else {
-            res.status(404).json({ message: "User not post" });
+            res.status(404).json({ message: "Your booking not successfully, please try again " });
         }
     } catch (error) {
         res.status(500).json({ message: error.message })
