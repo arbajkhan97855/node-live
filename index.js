b0bca94a97c1184eb86a7f397cc634c6dcd40635
arbajkhan97855 <pathanarbaj03328@gmail.com>
2025-08-01 21:36:28 +0530

    first commit

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ if (!fs.existsSync(uploadDir)) {
 
 const storage = multer.diskStorage({
     destination : (req,file,cb)=>{
-       cb(null,'./uploads')
+       cb(null, path.join(__dirname, "uploads"))
     },
     filename:(req,file,cb)=>{
         const newFileName = Date.now() + path.extname(file.originalname)
